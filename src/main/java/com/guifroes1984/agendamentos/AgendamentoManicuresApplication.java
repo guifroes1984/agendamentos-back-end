4b9f11fb7ae53e07d784488a458f4a9de720a3b0
guifroes1984 <guilhermefroes26@gmail.com>
2025-12-12 23:51:38 -0300

    feat: projeto base spring boot

---
+++ src/main/java/com/guifroes1984/agendamentos/AgendamentoManicuresApplication.java
@@ -0,0 +1,13 @@
+package com.guifroes1984.agendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoManicuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoManicuresApplication.class, args);
+	}
+
+}
